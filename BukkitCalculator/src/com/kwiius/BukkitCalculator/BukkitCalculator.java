281c776d344398dbe3235205fc89653ef0dc08c4
Jamie McClymont <jamie.mcclymont@gmail.com>
2013-01-31 21:30:23 +1300

    Things and stuff

--- BukkitCalculator/src/com/kwiius/BukkitCalculator/BukkitCalculator.java
+++ BukkitCalculator/src/com/kwiius/BukkitCalculator/BukkitCalculator.java
@@ -1,14 +1,17 @@
 package com.kwiius.BukkitCalculator;
 
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitCalculator extends JavaPlugin {
 	public static Logger log;
-	
+
 	@Override
 	public void onEnable() {
 		log = getLogger();
@@ -25,12 +28,31 @@ public class BukkitCalculator extends JavaPlugin {
 		if (cmd.getName().equals("c")) {
 			if (args.length != 0) {
 				String joined = Utils.implode(args);
+				
+				Pattern p = Pattern.compile("[a-zA-Z]");
+				Matcher m = p.matcher(joined);
+				if (m.find()) {
+					sender.sendMessage(ChatColor.RED + "Error: No letters allowed!");
+				}
+				
+				Pattern p2 = Pattern.compile("[.]");
+				Matcher m2 = p.matcher(joined);
+				if (m2.find()) {
+					sender.sendMessage(ChatColor.RED + "Error: Non-decimal numbers only!");
+				}
+				
+				joined.replace("**", "^");
+
 				try {
-					log.info(Calculator.Calculate(joined));
+					if(Calculator.Calculate(joined) == null) {
+						return false;
+					}
 				} catch (NumberFormatException e) {
-					e.printStackTrace();
+					sender.sendMessage(ChatColor.RED + "Error: "
+							+ e.getMessage());
 				} catch (Exception e) {
-					e.printStackTrace();
+					sender.sendMessage(ChatColor.RED + "Error: "
+							+ e.getMessage());
 				}
 			} else {
 				return false;
