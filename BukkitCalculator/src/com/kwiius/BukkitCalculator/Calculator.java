91486c6d4c5ef851fb5aeb2e6c5414cef0171c0d
printfn <printfn@users.noreply.github.com>
2013-01-31 22:09:19 +1300

    Fixed negative numbers

--- BukkitCalculator/src/com/kwiius/BukkitCalculator/Calculator.java
+++ BukkitCalculator/src/com/kwiius/BukkitCalculator/Calculator.java
@@ -45,7 +45,7 @@ public class Calculator {
 		Scanner s = new Scanner(calculation);
 		String token;
 		List<String> tokens = new ArrayList<String>();
-		while (null != (token = s.findInLine("-?\\d+|[+-/%^*()]")))
+		while (null != (token = s.findInLine("((?<=[0-9)])-)?\\d+|[+-/%^*()]")))
 		    tokens.add(token);
 		s.close();
 		CalculationObject[] result = new CalculationObject[tokens.size()];
