90e9fc1a4098d1620f9938ce6ab44610e91bfb1b
printfn <printfn@users.noreply.github.com>
2013-02-01 16:43:30 +1300

    Added stack-based evaluator

--- BukkitCalculator/src/com/kwiius/BukkitCalculator/Calculator.java
+++ BukkitCalculator/src/com/kwiius/BukkitCalculator/Calculator.java
@@ -7,10 +7,9 @@ import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bukkit.ChatColor;
-
 public class Calculator {
-	public static String calculate(String calculation) throws NumberFormatException, Exception {
+	public static String calculate(String calculation)
+			throws NumberFormatException, Exception {
 		Pattern p = Pattern.compile("[a-zA-Z]");
 		Matcher m = p.matcher(calculation);
 		if (m.find()) {
@@ -22,87 +21,140 @@ public class Calculator {
 		if (m2.find()) {
 			throw new Exception("No decimal numbers!");
 		}
-		
+
 		calculation = calculation.replace("**", "^");
-		
+
 		List<CalculationObject> rpn = shuntingYardAlgorithm(getTokenArray(calculation));
 		String[] strings = new String[rpn.size()];
 		for (int i = 0; i < rpn.size(); ++i) {
 			strings[i] = rpn.get(i).toString();
 		}
-		return Utils.implode(strings, " ");
+		BukkitCalculator.log.info(Utils.implode(strings, " "));
+		int result = calculateFromRPN(rpn);
+		return String.valueOf(result);
 	}
-	
-	private int calculateFromRPN(List<CalculationObject> rpn) {
+
+	private static int calculateFromRPN(List<CalculationObject> rpn) {
 		Stack<CalculationNumber> stack = new Stack<CalculationNumber>();
 		for (int i = 0; i < rpn.size(); ++i) {
+			CalculationObject token = rpn.get(i);
+			if (token.number != null) {
+				stack.push(token.number);
+			} else if (token.calculationOperator != null) {
+				int a, b, result;
+				switch (token.calculationOperator.operator) {
+				case '+':
+					a = stack.pop().number;
+					b = stack.pop().number;
+					result = b + a;
+					stack.push(new CalculationNumber(result));
+					break;
+				case '-':
+					a = stack.pop().number;
+					b = stack.pop().number;
+					result = b - a;
+					stack.push(new CalculationNumber(result));
+					break;
+				case '*':
+					a = stack.pop().number;
+					b = stack.pop().number;
+					result = b * a;
+					stack.push(new CalculationNumber(result));
+					break;
+				case '/':
+					a = stack.pop().number;
+					b = stack.pop().number;
+					result = b / a;
+					stack.push(new CalculationNumber(result));
+					break;
+				case '%':
+					a = stack.pop().number;
+					b = stack.pop().number;
+					result = b % a;
+					stack.push(new CalculationNumber(result));
+					break;
+				case '^':
+					a = stack.pop().number;
+					b = stack.pop().number;
+					result = b ^ a;
+					stack.push(new CalculationNumber(result));
+					break;
+				}
+			}
 		}
-		return 0;
+		return stack.pop().number;
 	}
-	
+
 	private static CalculationObject[] getTokenArray(String calculation)
-		throws NumberFormatException {
+			throws NumberFormatException {
 		Scanner s = new Scanner(calculation);
 		String token;
 		List<String> tokens = new ArrayList<String>();
-		while (null != (token = s.findInLine("((?<=[0-9)])-)?\\d+|[+-/%^*()]")))
-		    tokens.add(token);
+		while (null != (token = s.findInLine("((?<![0-9)])-)?\\d+|[+-/%^*()]")))
+			tokens.add(token);
 		s.close();
 		CalculationObject[] result = new CalculationObject[tokens.size()];
 		for (int i = 0; i < tokens.size(); ++i) {
 			String t = tokens.get(i);
-			if (t.equals("+") || t.equals("-") || t.equals("*") 
+			if (t.equals("+") || t.equals("-") || t.equals("*")
 					|| t.equals("/") || t.equals("%") || t.equals("^")) {
-				CalculationOperator calculationOperator = new CalculationOperator(t.charAt(0));
-				CalculationObject calculationObject = new CalculationObject(calculationOperator);
+				CalculationOperator calculationOperator = new CalculationOperator(
+						t.charAt(0));
+				CalculationObject calculationObject = new CalculationObject(
+						calculationOperator);
 				result[i] = calculationObject;
 			} else if (t.equals("(") || t.equals(")")) {
-				CalculationParenthesis calculationParenthesis = new CalculationParenthesis(t.equals("("));
-				CalculationObject calculationObject = new CalculationObject(calculationParenthesis);
+				CalculationParenthesis calculationParenthesis = new CalculationParenthesis(
+						t.equals("("));
+				CalculationObject calculationObject = new CalculationObject(
+						calculationParenthesis);
 				result[i] = calculationObject;
 			} else {
 				Integer number = Utils.parseInt(t);
 				if (number == null) {
-					throw new NumberFormatException("You did not enter a valid number (" + t + ").");
+					throw new NumberFormatException(
+							"You did not enter a valid number (" + t + ").");
 				}
-				CalculationNumber calculationNumber = new CalculationNumber(number);
-				CalculationObject calculationObject = new CalculationObject(calculationNumber);
+				CalculationNumber calculationNumber = new CalculationNumber(
+						number);
+				CalculationObject calculationObject = new CalculationObject(
+						calculationNumber);
 				result[i] = calculationObject;
 			}
 		}
 		return result;
 	}
-	
-	private static List<CalculationObject> shuntingYardAlgorithm(CalculationObject[] input) throws Exception {
+
+	private static List<CalculationObject> shuntingYardAlgorithm(
+			CalculationObject[] input) throws Exception {
 		int inputIdx = 0;
 		Stack<CalculationObject> stack = new Stack<CalculationObject>();
 		List<CalculationObject> output = new ArrayList<CalculationObject>();
-		
+
 		while (inputIdx < input.length) {
 			CalculationObject token = input[inputIdx++];
-			
+
 			if (token.number != null) {
 				output.add(token);
 				continue;
 			}
-			
+
 			if (token.calculationOperator != null) {
 				while ( // Operator on top of stack
-						(!stack.isEmpty() && stack.peek().calculationOperator != null)
-						// 
+				(!stack.isEmpty() && stack.peek().calculationOperator != null)
+				//
 						&& (
-							  // if token is left-associative and token.precedence <= stack.peek.precedence
-							  (token.calculationOperator.leftAssociative 
-									  && token.calculationOperator.precedence <= stack.peek().calculationOperator.precedence)
-							  // or token.precedence < stack.peek.precedence
-							  || (token.calculationOperator.precedence < stack.peek().calculationOperator.precedence)
-						   )
-					  ) {
+						// if token is left-associative and token.precedence <=
+						// stack.peek.precedence
+						(token.calculationOperator.leftAssociative && token.calculationOperator.precedence <= stack
+								.peek().calculationOperator.precedence)
+						// or token.precedence < stack.peek.precedence
+						|| (token.calculationOperator.precedence < stack.peek().calculationOperator.precedence))) {
 					output.add(stack.pop());
 				}
 				stack.push(token);
 			}
-			
+
 			if (token.parenthesis != null) {
 				if (token.parenthesis.leftParenthesis) {
 					stack.push(token);
@@ -110,8 +162,9 @@ public class Calculator {
 					if (stack.isEmpty()) {
 						throw new Exception("Mismatched parentheses!");
 					}
-					while (!stack.isEmpty() && 
-							(stack.peek().parenthesis == null || stack.peek().parenthesis.leftParenthesis == false)) {
+					while (!stack.isEmpty()
+							&& (stack.peek().parenthesis == null || stack
+									.peek().parenthesis.leftParenthesis == false)) {
 						output.add(stack.pop());
 					}
 					// pop left parenthesis
@@ -123,7 +176,7 @@ public class Calculator {
 				}
 			}
 		}
-		
+
 		// No more tokens to be read...
 		while (!stack.isEmpty()) {
 			CalculationObject token = stack.pop();
@@ -133,7 +186,7 @@ public class Calculator {
 				output.add(token);
 			}
 		}
-		
+
 		return output;
 	}
 }
